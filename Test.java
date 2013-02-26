c9572f6860bb9aef54ba52006f8fe01d77c65f29
abc <abc@123.com>
2013-02-26 14:00:11 +0800

    add Test.java

---
+++ Test.java
@@ -0,0 +1,48 @@
+class Example {
+    public int a;
+    int b;
+    
+    Example(){
+    	System.out.println("Example instance");
+    }
+    
+    Example(int x, int y){
+    	a = x;
+    	b = y;
+    }
+    
+    void setValue(int x, int y){
+    	a = x;
+    	b = y;
+    }
+    
+    public int getValue(){
+    	return b;
+    }
+}
+
+public class Test extends Example{
+	enum Color {RED, WHITE, BLACK};
+	
+    Test(int x, int y){
+    	a = x;
+    	b = y;
+    }
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+			System.out.println("try, return");
+			return;
+		}catch(Exception e){
+			System.out.println("Caught an exception");
+		}finally{
+			System.out.println("finally");
+		}
+
+	}
+
+}
