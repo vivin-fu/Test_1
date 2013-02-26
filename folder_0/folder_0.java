ad23669c2e1cf3ea0cc2cea8216132e322f0915c
abc <abc@123.com>
2013-02-26 15:13:17 +0800

    folder_0/folder_0.java

---
+++ folder_0/folder_0.java
@@ -0,0 +1,49 @@
+class Example {
+    public int a;
+    int b;
+//folder1
+    
+    Example(){
+    	System.out.println("Example instance");
+    }
+    
+    Example(int x, int y){
+    	a = x;
+    	b = y;
+    }
+    
+    void setValue(int x, int y){
+    	a = x;
+    	b = y;
+    }
+    
+    public int getValue(){
+    	return b;
+    }
+}
+
+public class Test extends Example{
+	enum Color {RED, WHITE, BLACK};
+	
+    Test(int x, int y){
+    	a = x;
+    	b = y;
+    }
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+			System.out.println("try, return");
+			return;
+		}catch(Exception e){
+			System.out.println("Caught an exception");
+		}finally{
+			System.out.println("finally");
+		}
+
+	}
+
+}
